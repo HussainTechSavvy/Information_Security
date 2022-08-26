39540e6c684727ac202e92565c55f95f1ecca4b7
hussain gandhi <gandhihussain515253@gmail.com>
2022-08-26 22:38:05 +0530

    Initial Commit

--- CompileAndRun-Linux_Mac/autoCompileAndRun.sh
+++ CompileAndRun-Linux_Mac/autoCompileAndRun.sh
@@ -1,3 +1,7 @@
+cd ..
+cd Code
 if g++ -o run printMenu.cpp getIntFromUser.cpp getIntFromAlphabet.cpp getAlphabetFromInt.cpp getExitStatus.cpp shiftCipher.cpp monoalphabaticCipher.cpp vernamCipher.cpp playfairCipher.cpp hillCipher.cpp transpositionCipher.cpp rsaAlgorithm.cpp diffieHellmanAlgorithm.cpp main.cpp; then
+mv run ../
+cd ..
 ./run
 fi
